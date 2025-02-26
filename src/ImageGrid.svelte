1f468ab0f45a3c9bbb0784defaca3871eca22953
Astraxy <ollearon2004@gmail.com>
2025-02-26 18:26:10 +0100

    asd

---
+++ src/ImageGrid.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    import anime from 'animejs';
+
+    onMount(() => {
+        anime({
+            targets: '#image-grid img',
+            scale: '1'
+        });
+    })
+
+    const randomImage = (): string => {
+        return `https://picsum.photos/200/200?random=${Math.random()}`
+    }
+</script>
+
+<div class="rotate-3d">
+    <div id="image-grid" class="grid grid-cols-3 gap-3">
+        {#each {length: 3*3}}
+            <img 
+                class="shadow-2xl"
+                style="transform: scale(0);"
+                src={randomImage()} 
+                alt="A beautiful pic!"
+            >
+        {/each}
+    </div>
+</div>
+
+
+<style>
+    :global(.rotate-3d) {
+        transform: perspective(100rem) rotateY(-15deg);
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { Badge } from "$lib/components/ui/badge/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+
+    import ImageGrid from "../ImageGrid.svelte";
+</script>
+
+<div class="h-lvh flex justify-center items-center">
+    <div class="flex w-1/3 h-1/2">
+        <div class="basis-2/3">
+            <div class="my-3">
+                <h1 class="text-7xl font-bold">screenie</h1>
+                <Badge>beta</Badge>
+            </div>
+            <p class="text-gray-500 text-lg my-3">share your screenshots and videos easily</p>
+            <Button>get started</Button>
+        </div>
+        <div>
+            <ImageGrid/>
+        </div>
+    </div>
+</div>
